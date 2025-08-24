a478cfdf804e06c8bf2c0e313db4da62d1456199
ujjwal <ug21u03027@gmail.com>
2025-08-25 02:22:42 +0530

    auth feature

--- hospital-appointment-backend/src/main/java/com/teame/hospital_appointment_backend/dao/UserDao.java
+++ hospital-appointment-backend/src/main/java/com/teame/hospital_appointment_backend/dao/UserDao.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 public interface UserDao extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 
     boolean existsByUsername(String username);
 
